33eb6f61fdc303dc11dccaa984e2a4086dbb2d1b
Pavel <vondrasekcz@gmail.com>
2019-01-20 13:09:41 +0100

    responsive UI :D:D

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -42,6 +42,16 @@ const Layout = ({
           width: 70%;
           margin: 0 auto;
         }
+
+        @media screen and (max-width: 992px) {
+          .content {
+            width: 100%;
+          }
+  
+          .appbar {
+            width: 100%;
+          }
+        }
       `
     }
     </style>
